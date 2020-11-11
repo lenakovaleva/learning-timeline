c58167fd048fea742178f0fd4ac5e04d6cbfdb87
Lena Kovaleva <insolence14@gmail.com>
2020-11-11 15:46:00 +0700

    First commit

---
+++ src/Item.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div v-for="milestone in milestones" v-bind:key="milestone.id"
+        class="card" style="width: auto"
+        v-bind:class="milestone.label"
+        >
+  <img v-bind:src="milestone.img" class="card-img-top">
+  <div class="card-body">
+    <a v-bind:href="milestone.url" class="card-link"><h5 class="card-title">{{ milestone.title }}</h5></a>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  {{ milestones }}
+</div>
+        <Data 
+        v-bind:milestones="milestones"
+        />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Item',
+     data () {
+    return {
+      milestones: [
+    {
+        id: 1,
+        label: 'course',
+        title: 'Title',
+        url: 'URL',
+        img: 'Image',
+    },
+    {
+        id: 2,
+        label: 'course',
+        title: 'Title2',
+        url: 'URL2',
+        img: 'Image2',
+    },
+],
+    }
+  }
+  
+}
+</script>
---
+++ src/Timeline.vue
@@ -0,0 +1,32 @@
+<template>
+ <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+         <div class="wrapper">
+           <Item />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Item from "./Item.vue"
+
+export default {
+  name: 'Timeline',
+    components: {
+    Item
+  }, 
+  props: {
+    msg: String
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
